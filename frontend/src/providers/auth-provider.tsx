6fac5f2b939effe44e5494ef342aa8d104fbaf8a
Nate <nate@calculated.fi>
2025-05-02 16:10:56 +1000

    fix signup ux and errors

--- frontend/src/providers/auth-provider.tsx
+++ frontend/src/providers/auth-provider.tsx
@@ -1,14 +1,14 @@
-import { useStorage } from "@/helpers/storage";
+import { useStorage } from '@/helpers/storage';
 import {
   LoginCredentials,
   SignupCredentials,
   useLogin,
   useSignup,
-} from "@/hooks/use-auth";
-import { UserDto, useUser } from "@/hooks/use-user";
-import { useNavigate } from "@tanstack/react-router";
-import { createContext, useEffect, useState } from "react";
-import { toast } from "sonner";
+} from '@/hooks/use-auth';
+import { UserDto, useUser } from '@/hooks/use-user';
+import { useNavigate } from '@tanstack/react-router';
+import { createContext, useEffect, useState } from 'react';
+import { toast } from 'sonner';
 
 export const AuthContext = createContext<{
   user: UserDto | undefined;
@@ -20,6 +20,10 @@ export const AuthContext = createContext<{
   signup: (credentials: SignupCredentials) => void;
   login: (credentials: LoginCredentials) => void;
   logout: () => void;
+  isLoginLoading: boolean;
+  isSignupLoading: boolean;
+  loginError: Error | null;
+  signupError: Error | null;
 }>({
   user: undefined,
   setUser: () => {},
@@ -30,44 +34,91 @@ export const AuthContext = createContext<{
   signup: () => {},
   login: () => {},
   logout: () => {},
+  isLoginLoading: false,
+  isSignupLoading: false,
+  loginError: null,
+  signupError: null,
 });
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [user, setUser] = useState<UserDto | undefined>();
-  const [isUserLoading, setIsUserLoading] = useState(true);
-  const [userError, setUserError] = useState<Error | null>(null);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
+  const [isUserLoading, setIsUserLoading] = useState(true);
+  const [isLoginLoading, setIsLoginLoading] = useState(false);
+  const [isSignupLoading, setIsSignupLoading] = useState(false);
+
+  const [userError, setUserError] = useState<Error | null>(null);
+  const [loginError, setLoginError] = useState<Error | null>(null);
+  const [signupError, setSignupError] = useState<Error | null>(null);
+
   const { getItem, removeItem, clearStorage } = useStorage();
-  const { data: currUser, isPending, error, refetch } = useUser();
-  const { mutate: signup } = useSignup();
-  const { mutate: login } = useLogin();
+  const {
+    data: currentUser,
+    isPending: isCurrentUserPending,
+    error: currentUserError,
+    refetch: refetchCurrentUser,
+  } = useUser();
+
+  const {
+    mutate: signupMutate,
+    isPending: isSignupPending,
+    error: signupFetchError,
+  } = useSignup();
+
+  const {
+    mutate: loginMutate,
+    isPending: isLoginPending,
+    error: loginFetchError,
+  } = useLogin();
 
   const navigate = useNavigate();
 
   const logout = () => {
-    removeItem("token");
-    removeItem("user");
-    clearStorage("session");
+    removeItem('token');
+    removeItem('user');
+    clearStorage('session');
     setUser(undefined);
     setIsAuthenticated(false);
     setUserError(null);
-    toast("Logged out successfully", {
-      icon: "✅",
-      description: "Adios, see you next time!",
+    setLoginError(null);
+    setSignupError(null);
+    toast('Logged out successfully', {
+      icon: '✅',
+      description: 'Adios, see you next time!',
       duration: 3000,
       action: {
-        label: "Close",
+        label: 'Close',
         onClick: () => {
           toast.dismiss();
         },
       },
     });
-    navigate({ to: "/auth" });
+    navigate({ to: '/auth' });
+  };
+
+  const login = (credentials: LoginCredentials) => {
+    setIsLoginLoading(true);
+    setLoginError(null);
+    loginMutate(credentials, {
+      onSettled: () => {
+        setIsLoginLoading(false);
+      },
+    });
+  };
+
+  const signup = (credentials: SignupCredentials) => {
+    setIsSignupLoading(true);
+    setSignupError(null);
+    signupMutate(credentials, {
+      onSettled: () => {
+        setIsSignupLoading(false);
+      },
+    });
   };
 
   useEffect(() => {
-    const token = getItem("token");
+    const token = getItem('token');
     if (!token) {
       setUser(undefined);
       setIsAuthenticated(false);
@@ -76,53 +127,76 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
       return;
     }
 
-    if (isPending) {
-      return;
-    }
+    setIsUserLoading(isCurrentUserPending);
 
-    if (error) {
-      console.error("Error fetching user data:", error);
-      setUserError(error instanceof Error ? error : new Error(String(error)));
+    if (currentUserError) {
+      const error =
+        currentUserError instanceof Error
+          ? currentUserError
+          : new Error(String(currentUserError));
+
+      console.error('Error fetching user data:', error);
+      setUserError(error);
       logout();
-      setIsUserLoading(false);
       return;
     }
 
-    if (currUser) {
-      setUser(currUser);
+    if (currentUser) {
+      setUser(currentUser);
       setIsAuthenticated(true);
       setUserError(null);
-    } else {
+    } else if (!isCurrentUserPending) {
       logout();
     }
+  }, [currentUser, isCurrentUserPending, currentUserError, login, signup]);
 
-    setIsUserLoading(false);
-  }, [currUser, isPending, error, getItem]);
+  useEffect(() => {
+    if (loginFetchError) {
+      setLoginError(loginFetchError);
+    }
+
+    if (!isLoginPending) {
+      setIsLoginLoading(false);
+    }
+  }, [isLoginPending, loginFetchError]);
+
+  useEffect(() => {
+    if (signupFetchError) {
+      setSignupError(signupFetchError);
+    }
+
+    if (!isSignupPending) {
+      setIsSignupLoading(false);
+    }
+  }, [isSignupPending, signupFetchError]);
 
   useEffect(() => {
     if (!isAuthenticated) return;
 
     const refreshInterval = setInterval(
       () => {
-        refetch();
+        refetchCurrentUser();
       },
       15 * 60 * 1000
     ); // 15 minutes
 
     return () => clearInterval(refreshInterval);
-  }, [isAuthenticated, refetch]);
+  }, [isAuthenticated, refetchCurrentUser]);
 
   const value = {
     user,
     setUser,
     isUserLoading,
     setIsUserLoading,
-    isAuthenticated,
     userError,
     setUserError,
     signup,
     login,
     logout,
+    isLoginLoading,
+    isSignupLoading,
+    loginError,
+    signupError,
   };
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
--- frontend/src/routes/auth.tsx
+++ frontend/src/routes/auth.tsx
@@ -6,6 +6,19 @@ import { Label } from '@/components/ui/label';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { AuthContext } from '@/providers/auth-provider';
 import { LoginCredentials } from '@/hooks/use-auth';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { BadgeCheck, BadgeX } from 'lucide-react';
+import { cn } from '@/lib/utils';
 
 export const Route = createFileRoute('/auth')({
   component: RouteComponent,
@@ -39,8 +52,7 @@ const LoginForm = () => {
     email: '',
     password: '',
   });
-
-  const { user, login, userError, isUserLoading } = useContext(AuthContext);
+  const { user, login, userError, isLoginLoading } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -96,8 +108,8 @@ const LoginForm = () => {
             </div>
           )}
 
-          <Button type="submit" className="w-full" disabled={isUserLoading}>
-            {isUserLoading ? 'Logging in...' : 'Login'}
+          <Button type="submit" className="w-full" disabled={isLoginLoading}>
+            {isLoginLoading ? 'Logging in...' : 'Login'}
           </Button>
         </form>
       </CardContent>
@@ -106,31 +118,51 @@ const LoginForm = () => {
 };
 
 const SignupForm = () => {
-  const [formData, setFormData] = useState({
-    username: '',
-    email: '',
-    password: '',
-  });
+  const signupSchema = z
+    .object({
+      username: z.string().min(1, 'Username is required'),
+      email: z.string().email('Invalid email address'),
+      password: z
+        .string()
+        .min(8, 'Password must be at least 8 characters')
+        .regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
+        .regex(/[a-z]/, 'Password must contain at least one lowercase letter')
+        .regex(/\d/, 'Password must contain at least one number')
+        .regex(
+          /[@$!%*?&#]/,
+          'Password must contain at least one special character'
+        ),
+      confirmPassword: z.string().min(8, 'Confirm Password is required'),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+      message: "Passwords don't match",
+      path: ['confirmPassword'],
+    });
 
-  const { user, signup, userError, isUserLoading } = useContext(AuthContext);
-  const navigate = useNavigate();
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData((prev) => ({ ...prev, [name]: value }));
-  };
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    signup(formData);
-    setFormData({
+  const form = useForm({
+    resolver: zodResolver(signupSchema),
+    defaultValues: {
       username: '',
       email: '',
       password: '',
+      confirmPassword: '',
+    },
+  });
+
+  const { user, signup, isUserLoading, signupError } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleSubmit = () => {
+    const { username, email, password } = form.getValues();
+    signup({
+      username,
+      email,
+      password,
     });
     if (user) {
       navigate({ to: '/dashboard' });
     }
+    form.reset();
   };
 
   return (
@@ -139,52 +171,179 @@ const SignupForm = () => {
         <CardTitle>Create an Account</CardTitle>
       </CardHeader>
       <CardContent>
-        <form onSubmit={handleSubmit} className="space-y-4">
-          <div className="space-y-2">
-            <Label htmlFor="username">Username</Label>
-            <Input
-              type="text"
-              id="username"
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(handleSubmit)}
+            className="space-y-4"
+          >
+            <FormField
               name="username"
-              value={formData.username}
-              onChange={handleChange}
-              required
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel htmlFor="username">Username</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        type="text"
+                        id="username"
+                        name="username"
+                        required
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
             />
-          </div>
 
-          <div className="space-y-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              type="email"
-              id="email"
+            <FormField
               name="email"
-              value={formData.email}
-              onChange={handleChange}
-              required
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel htmlFor="email">Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        type="email"
+                        id="email"
+                        name="email"
+                        required
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
             />
-          </div>
 
-          <div className="space-y-2">
-            <Label htmlFor="password">Password</Label>
-            <Input
-              type="password"
-              id="password"
+            <FormField
               name="password"
-              value={formData.password}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          {userError && (
-            <div className="rounded-md bg-red-100 p-4 text-red-800">
-              <h3>{userError.message}</h3>
-            </div>
-          )}
+              render={({ field }) => {
+                const password = form.watch('password');
 
-          <Button type="submit" className="w-full" disabled={isUserLoading}>
-            {isUserLoading ? 'Creating Account...' : 'Sign Up'}
-          </Button>
-        </form>
+                const requirements = [
+                  {
+                    label: 'At least 8 characters',
+                    test: (pw: string) => pw.length >= 8,
+                  },
+                  {
+                    label: 'At least one uppercase letter',
+                    test: (pw: string) => /[A-Z]/.test(pw),
+                  },
+                  {
+                    label: 'At least one lowercase letter',
+                    test: (pw: string) => /[a-z]/.test(pw),
+                  },
+                  {
+                    label: 'At least one number',
+                    test: (pw: string) => /\d/.test(pw),
+                  },
+                  {
+                    label: 'At least one special character',
+                    test: (pw: string) => /[@$!%*?&#]/.test(pw),
+                  },
+                ];
+
+                return (
+                  <FormItem>
+                    <FormLabel htmlFor="password">Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        type="password"
+                        id="password"
+                        name="password"
+                        required
+                      />
+                    </FormControl>
+                    {field.value && (
+                      <ul className="mt-2 space-y-1">
+                        {requirements.map((req, index) => {
+                          const isValid = req.test(password || '');
+                          return (
+                            <li
+                              key={index}
+                              className={cn(
+                                `flex items-center space-x-2 text-xs`,
+                                isValid ? 'text-green-500' : 'text-red-500'
+                              )}
+                            >
+                              {isValid ? (
+                                <BadgeCheck className="h-4 w-4" />
+                              ) : (
+                                <BadgeX className="h-4 w-4" />
+                              )}
+                              <span>{req.label}</span>
+                            </li>
+                          );
+                        })}
+                      </ul>
+                    )}
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
+            />
+            <FormField
+              name="confirmPassword"
+              render={({ field }) => {
+                const password = form.watch('password');
+                const confirmPassword = form.watch('confirmPassword');
+
+                return (
+                  <FormItem>
+                    <FormLabel htmlFor="confirm-password">
+                      Confirm Password
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        type="password"
+                        id="confirm-password"
+                        name="confirmPassword"
+                        required
+                      />
+                    </FormControl>
+                    {password &&
+                      confirmPassword &&
+                      password !== confirmPassword && (
+                        <FormMessage>
+                          <span className="text-red-500">
+                            Passwords do not match
+                          </span>
+                        </FormMessage>
+                      )}
+                    {password &&
+                      confirmPassword &&
+                      password === confirmPassword && (
+                        <FormMessage>
+                          <span className="text-green-500">
+                            Passwords match
+                          </span>
+                        </FormMessage>
+                      )}
+                  </FormItem>
+                );
+              }}
+            />
+
+            {signupError && (
+              <div className="rounded-md bg-red-100 p-4 text-red-800">
+                <h3>{signupError.message}</h3>
+              </div>
+            )}
+
+            <Button
+              type="submit"
+              className="w-full"
+              disabled={isUserLoading || !form.formState.isValid}
+            >
+              {isUserLoading ? 'Creating Account...' : 'Sign Up'}
+            </Button>
+          </form>
+        </Form>
       </CardContent>
     </Card>
   );
